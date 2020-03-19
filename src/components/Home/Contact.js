490f15780580f846093a8e9ba536c3396b55bd6a
Ad Rienks <ad.rienks@gmail.com>
2020-03-19 04:22:36 +0100

    Snipcart setup

--- src/components/Home/Contact.js
+++ src/components/Home/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
       <Title title="contact us" />
       <div className="row">
         <div className="col-10 col-sm-8 col-md-6 mx-auto">
-          <form>
+          <form action="https://formspree.io/mlelljrp" method="POST">
             {/* name */}
             <div className="form-group">
               <label htmlFor="name">Name</label>
--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -9,8 +9,15 @@ export default function Product({ product }) {
         <div className="card-body text-center">
           <h6>{product.title}</h6>
           <h6>€ {product.price.toFixed(2)}</h6>
-          <button className="btn btn-yellow mt-3 text-capitalize">
-            add to cart
+          <button
+            className="btn btn-yellow mt-3 text-capitalize snipcart-add-item"
+            data-item-id={product.id}
+            data-item-name={product.title}
+            data-item-price={product.price}
+            data-item-image={product.image.fluid.src}
+            data-item-url="https://kwi-gatsby-coffee-project-recording.netlify.com/"
+          >
+            > add to cart
           </button>
         </div>
       </div>
